672479439e4fe294212aab6d89f711ba6e6305b5
Shahm Barmada <barmada.sha@gmail.com>
2024-04-24 03:03:27 +0200

    set configModule gloabl

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 import { UsersModule } from './users/users.module';
@@ -19,6 +20,7 @@ import { VouchersModule } from './vouchers/vouchers.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     DatabaseModule,
     UsersModule,
     RolesModule,
